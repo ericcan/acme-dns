65a2095f9a04b8b80730ad7cb771390bfcedf2d2
Eric Candell <eric@candell.org>
2019-03-22 08:43:23 -0700

    Added the calling IP to the DNSServer object rather than passing as parameter

--- dns.go
+++ dns.go
@@ -19,6 +19,7 @@ type DNSServer struct {
 	Server  *dns.Server
 	SOA     dns.RR
 	Domains map[string]Records
+	LastIP  string
 }
 
 // NewDNSServer parses the DNS records from config and returns a new DNSServer struct
@@ -83,16 +84,17 @@ func (d *DNSServer) handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 	m.SetReply(r)
 
 	if r.Opcode == dns.OpcodeQuery {
-		d.readQuery(m,w.RemoteAddr().String())
+		d.LastIP=w.RemoteAddr().String()
+		d.readQuery(m)
 	}
 	w.WriteMsg(m)
 
 }
 
-func (d *DNSServer) readQuery(m *dns.Msg, ip string) {
+func (d *DNSServer) readQuery(m *dns.Msg) {
 	var authoritative = false
 	for _, que := range m.Question {
-		if rr, rc, auth, err := d.answer(que,ip); err == nil {
+		if rr, rc, auth, err := d.answer(que); err == nil {
 			if auth {
 				authoritative = auth
 			}
@@ -151,7 +153,7 @@ func (d *DNSServer) isAuthoritative(q dns.Question) bool {
 	return false
 }
 
-func (d *DNSServer) answer(q dns.Question,ip string) ([]dns.RR, int, bool, error) {
+func (d *DNSServer) answer(q dns.Question) ([]dns.RR, int, bool, error) {
 	var rcode int
 	var authoritative = d.isAuthoritative(q)
 	if !d.answeringForDomain(q.Name) {
@@ -174,7 +176,7 @@ func (d *DNSServer) answer(q dns.Question,ip string) ([]dns.RR, int, bool, error
 	if q.Qtype == dns.TypeOPT {
 		return []dns.RR{}, dns.RcodeFormatError, authoritative, nil
 	}
-	log.WithFields(log.Fields{"qtype": dns.TypeToString[q.Qtype], "domain": q.Name, "ip": ip,"rcode": dns.RcodeToString[rcode]}).Debug("DNS:Answering question for domain")
+	log.WithFields(log.Fields{"qtype": dns.TypeToString[q.Qtype], "domain": q.Name, "ip": d.LastIP,"rcode": dns.RcodeToString[rcode]}).Debug("DNS:Answering question for domain")
 	return r, rcode, authoritative, nil
 }
 
